29efc1344cf737891f093b65a7db03f3494b238d
Van Doan <vdoan09@gmail.com>
2015-08-04 23:04:03 -0400

    sand box 2

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -3,3 +3,16 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var stri : String = "Hello"
+
+let modernProgrammingLanguage: String = "Swift"
+
+println("There once were three lions in a cage")
+
+print(modernProgrammingLanguage)
+
+println("A panda walks into a bar \(modernProgrammingLanguage)")
+
+
+println("List: \n 1\tapple\n2\tcookies\n3\thotdogs")
