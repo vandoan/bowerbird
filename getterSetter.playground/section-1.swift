1222efa5f116ccb1077110ce4336c22bb5a477d2
Van Doan <vdoan09@gmail.com>
2015-08-26 22:59:36 -0400

    getter setter1

--- getterSetter.playground/section-1.swift
+++ getterSetter.playground/section-1.swift
@@ -45,3 +45,17 @@ table.surfaceArea = 144
 table.width
 table.length
 table.surfaceArea
+
+
+
+class Temperature {
+    var celsius: Float = 0.0
+    var fahrenheit: Float {
+        get {
+            return (celsius * 1.8) + 32.0
+        }
+        set {
+            celsius = (newValue - 32) / 1.8
+        }
+    }
+}
